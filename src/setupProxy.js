bad483e10dd7a44688356adffa0b2f4ee24e06e3
Haider Mansoor <haider@bitfactory.io>
2023-04-29 20:53:56 +0300

    v1.0

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function (app) {
-  app.use(
-    ['/api', '/auth/google'],
-    createProxyMiddleware({
-      target: 'https://emaily-server-production.up.railway.app',
-    })
-  );
-};
