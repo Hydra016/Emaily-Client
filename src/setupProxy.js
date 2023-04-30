bbce438b4469f3eb12b6119c3958917957f3c9c4
Haider Mansoor <haider@bitfactory.io>
2023-05-01 00:01:27 +0300

    v1.0

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,11 +4,8 @@ module.exports = function (app) {
     ['/api', '/auth/google'],
     createProxyMiddleware({
       target: 'https://emaily-server-production.up.railway.app',
-      changeOrigin: true,
-      onProxyRes: function (proxyRes, req, res) {
-        proxyRes.headers['Content-Encoding'] = 'gzip';
-        proxyRes.headers['Transfer-Encoding'] = 'chunked';
-        proxyRes.pipe(zlib.createGzip()).pipe(res);
+      headers: {
+        host: 'emaily-server-production.up.railway.app'
       }
     })
   );
